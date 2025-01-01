e1f8ca89f41a0af9a277d5133cd1ee93ea782c0b
lixanm <143246847+lixanm@users.noreply.github.com>
2025-01-01 18:19:18 +0800

    做了飞行的动画和开始界面鸟的动画

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -6,18 +6,21 @@ public class Bird : MonoBehaviour
 {
     public int UPSpeed;
     private Rigidbody2D myrigidbody2D;
+    private Animator animator;
 
-    // Start is called before the first frame update
     void Start()
     {
         UPSpeed = 5;
         myrigidbody2D = GetComponent<Rigidbody2D>();
+        animator = transform.Find("birdShow").GetComponent<Animator>();
+
+        animator.SetInteger("state", 1);
+        myrigidbody2D.simulated = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //0�����1�Ҽ�
         if(Input.GetMouseButtonDown(0)==true)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, UPSpeed);
